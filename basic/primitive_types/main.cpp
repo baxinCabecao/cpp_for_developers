33b0c1f96aae7045c3a44a9e234cf672a2f20eac
F4613569 Jakson de Oliveira Lima <jaksonol@bb.com.br>
2023-09-24 15:18:04 -0300

    inicio do projeto

---
+++ basic/primitive_types/main.cpp
@@ -0,0 +1,77 @@
+// Include necessary headers.
+#include <iostream>  
+#include <limits>    // This header provides information about data types.
+
+using namespace std;
+
+int main() {
+
+    // C++ offers several basic built-in data types.
+
+    // **Integer types**
+    
+
+    int integerVar = 10;
+    cout << "int size: " << sizeof(integerVar) << " bytes." << endl;
+    cout << "int max value: " << numeric_limits<int>::max() << endl;
+    cout << "int min value: " << numeric_limits<int>::min() << endl;
+
+    short shortVar = 32000;  // Declare a short integer.
+    cout << "short size: " << sizeof(shortVar) << " bytes." << endl;
+    cout << "short max value: " << numeric_limits<short>::max() << endl;
+    cout << "short min value: " << numeric_limits<short>::min() << endl;
+
+    long longVar = 100000000L;  // Declare a long integer.
+    cout << "long size: " << sizeof(longVar) << " bytes." << endl;
+    cout << "long max value: " << numeric_limits<long>::max() << endl;
+    cout << "long min value: " << numeric_limits<long>::min() << endl;
+
+    long long longLongVar = 10000000000000000LL;  // Declare a long long integer.
+    cout << "long long size: " << sizeof(longLongVar) << " bytes." << endl;
+    cout << "long long max value: " << numeric_limits<long long>::max() << endl;
+    cout << "long long min value: " << numeric_limits<long long>::min() << endl;
+
+    unsigned int unsignedIntVar = 4000000000U;  // Declare an unsigned integer.
+    cout << "unsigned int size: " << sizeof(unsignedIntVar) << " bytes." << endl;
+    cout << "unsigned int max value: " << numeric_limits<unsigned int>::max() << endl;
+    cout << "unsigned int min value: 0 (unsigned always non-negative)" << endl;
+
+    // **Floating point types**
+
+    float floatVar = 3.14f; // Declare a floating-point variable.
+    cout << "float size: " << sizeof(floatVar) << " bytes." << endl;
+    cout << "float max value: " << numeric_limits<float>::max() << endl;
+    cout << "float min value: " << numeric_limits<float>::min() << endl;
+
+    double doubleVar = 3.141592653589793;  // 'double' is like 'float' but with double the precision.
+    cout << "double size: " << sizeof(doubleVar) << " bytes." << endl;
+
+    // **Character type**
+
+    char charVar = 'A';  // Declare a character variable.
+    cout << "char size: " << sizeof(charVar) << " bytes." << endl;
+    cout << "char max value: " << (int)numeric_limits<char>::max() << endl;  // Cast char to int for display.
+    cout << "char min value: " << (int)numeric_limits<char>::min() << endl;
+
+    unsigned char unsignedCharVar = 'A';  // Declare a character variable.
+    cout << "unsigned char size: " << sizeof(unsignedCharVar) << " bytes." << endl;
+    cout << "unsigned char max value: " << (int)numeric_limits<unsigned char>::max() << endl;  // Cast char to int for display.
+    cout << "unsigned char min value: " << (int)numeric_limits<unsigned char>::min() << endl;
+
+    // **Boolean type**
+
+    bool boolVar = true;  // Declare a boolean variable. It can be 'true' or 'false'.
+    cout << "bool size: " << sizeof(boolVar) << " bytes." << endl;
+
+    // **Void type**
+
+    // The 'void' type represents the absence of value. Commonly used with functions to indicate no return value.
+    // Can't declare a variable of type 'void'.
+
+    // **Wide character type**
+
+    wchar_t wideCharVar = L'A';  // Declare a wide character. It's larger than 'char' and can represent more characters.
+    cout << "wchar_t size: " << sizeof(wideCharVar) << " bytes." << endl;
+
+    return 0;
+}
---
+++ basic/syntax_basic/main.cpp
@@ -0,0 +1,35 @@
+// This is a single-line comment in C++
+
+/*
+   This is a multi-line comment in C++
+   It can span several lines.
+*/
+
+// The '#include' directive is used to include headers.
+#include <iostream>
+
+// 'using namespace' tells the compiler to use names from the specified namespace without prefixing.
+using namespace std;  // Use the standard namespace. It contains common functions like 'cout'.
+
+int main()
+{
+    // The curly braces '{}' define a scope. In this case, they define the body of the 'main' function.
+    
+    cout << "Hello, World!" << std::endl;  // 'cout' is used to output to the console. 
+                                      // '<<' is the insertion operator.
+                                      // 'endl' is an end-of-line character.
+
+    int x = 5;  // Declare an integer variable named 'x' and assign it a value of 5.
+
+    if (x > 3) {  // 'if' is a conditional statement. The condition inside '()' is evaluated.
+        // If the condition is true, the code inside the following '{}' is executed.
+        cout << "x is greater than 3" << endl;
+    }
+
+    for (int i = 0; i < 3; i++) {  // A 'for' loop. It has three parts: initialization, condition, and iteration.
+        cout << "Loop iteration: " << i << endl;  // Display the value of 'i' in each iteration.
+    }
+
+    // Every statement in C++ ends with a semicolon ';'. It signifies the end of a command.
+    return 0;
+}
